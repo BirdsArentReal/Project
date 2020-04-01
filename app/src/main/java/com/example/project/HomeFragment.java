b1688bf922d376bde81c2e3b05431188d0377d39
BirdsArentReal <rsyj04@gmail.com>
2020-04-01 11:56:18 +0800

    home fragment layout done

--- app/src/main/java/com/example/project/HomeFragment.java
+++ app/src/main/java/com/example/project/HomeFragment.java
@@ -7,9 +7,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+    CardView calendar, links, food, health;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
