8b69d902366fd80df85eaedd863d9a31e9d7f190
BirdsArentReal <rsyj04@gmail.com>
2020-04-02 14:50:13 +0800

    some extra basic coding

---
+++ app/src/main/java/com/example/project/ReminderDialog.java
@@ -0,0 +1,37 @@
+package com.example.project;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ReminderDialog extends AppCompatDialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.dialog_add_reminder, null);
+
+        builder.setView(view);
+        builder.setTitle("Add Reminder");
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/project/ReminderTab.java
+++ app/src/main/java/com/example/project/ReminderTab.java
@@ -4,17 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ReminderTab extends Fragment{
+        Button add;
 
         @Nullable
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View view = inflater.inflate(R.layout.tab_reminder, container, false);
+            add = view.findViewById(R.id.reminderButton);
+
+            add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    ReminderDialog reminderDialog = new ReminderDialog();
+
+                    reminderDialog.show(getActivity().getSupportFragmentManager(),"Reminder dialog");
+                }
+            });
             return view;
         }
 }
